306c1a3b92b63bc5524d8069276b2d072dcb73b4
Michael <mqngo21@gmail.com>
2018-03-02 17:31:27 -0800

    made adjustments: class to className

--- react/src/overallRatings.jsx
+++ react/src/overallRatings.jsx
@@ -12,18 +12,18 @@ const OverallRatings = (props) => {
             <div className='overallrating'>
               <span className='graph'>
                 <ul className='bargraph'>
-                  5<li class="reddeep"></li>
-                  4<li class="redpink"></li>
-                  3<li class="pink"></li>
-                  2<li class="orangered"></li>
-                  1<li class="orange"></li>
+                  5<li className="reddeep"></li>
+                  4<li className="redpink"></li>
+                  3<li className="pink"></li>
+                  2<li className="orangered"></li>
+                  1<li className="orange"></li>
                 </ul>
               </span>
               <span className='ratingNum'>4.5</span>
               <span className='overall'>Overall Rating</span>
               <div className='stars'><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div>
               <div className='mood'><span>Food: 4.7</span> | <span>Service: 4.5</span> | <span>Ambience: 4.0</span> | <span>Value: 4.2</span>  <span>Noise: Hella Loud</span></div>
-              <div className='recommendText'><i class="glyphicon glyphicon-thumbs-up"></i> 96% would recommend it to a friend</div>
+              <div className='recommendText'><i className="glyphicon glyphicon-thumbs-up"></i> 96% would recommend it to a friend</div>
               <div className='reviewsCount'>928 reviews</div>
             </div>
           </div>
