42fa1e2a994702f8868d52fad5adbc1dbe3e0d0f
Michael <mqngo21@gmail.com>
2018-03-06 15:38:13 -0800

    corrected seeding disconnect

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ const reviewsList = require('./sample_reviews.js');
 const mongoose = require('mongoose');
 const db = require('./db/mongodb.js');
 
-mongoose.connect('mongodb://localhost/restaurantsData');
+// mongoose.connect('mongodb://localhost/restaurantsData');
 
 const getReviews = function () {
   // random length for how many reviews will render with minimum of 3 reviews
@@ -20,44 +20,23 @@ const getReviews = function () {
   return reviewsArray;
 };
 
-// const seedDb = function (restaurantsData) {
-//   // looping thru restaurants
-//   restaurantsData.forEach((restaurant) => {
-//     // for a restaurant, use schema to set values
-//     const eachRestaurant = {
-//       restaurantId: restaurant.id,
-//       restaurantName: restaurant.name,
-//       restaurantReviews: getReviews(),
-//     };
-//     // save each one into db
-//     db.insertOne(eachRestaurant, (error) => {
-//       if (error) {
-//         throw error;
-//       }
-//     });
-//   });
-// };
-
-const seedDb = function (restaurantsData) {
-  for (var i = 0; i < restaurantsData.length; i++) {
-    const restaurant = restaurantsData[i];
-    const eachRestaurant = {
-      restaurantId: restaurant.id,
-      restaurantName: restaurant.name,
-      restaurantReviews: getReviews(),
-    };
-    // save each one into db
-    db.insertOne(eachRestaurant, (error) => {
-      if (error) {
-        throw error;
+let counter = 0;
+restaurantsData.forEach((restaurant) => {
+  // for a restaurant, use schema to set values
+  const eachRestaurant = {
+    restaurantId: restaurant.id,
+    restaurantName: restaurant.name,
+    restaurantReviews: getReviews(),
+  };
+  // save each one into db
+  db.insertOne(eachRestaurant, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      counter++;
+      if (counter === 119) {
+        mongoose.disconnect();
       }
-    });
-    if (i === restaurantsData.length - 1) {
-      mongoose.disconnect();
     }
-  }
-};
-
-// seed data into db
-seedDb(restaurantsData);
-
+  });
+});
