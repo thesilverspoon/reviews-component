1d06ff15ab57e3f22bfdbeca9aed9c2bfb619db7
Michael <mqngo21@gmail.com>
2018-03-06 14:51:33 -0800

    update seeding

--- seed.js
+++ seed.js
@@ -5,30 +5,30 @@ const db = require('./db/mongodb.js');
 
 mongoose.connect('mongodb://localhost/restaurantsData');
 
-let getReviews = function () {
+const getReviews = function () {
   // random length for how many reviews will render with minimum of 3 reviews
-  let randomLength = Math.ceil(Math.random() * 10) + 3;
-  let reviewsArray = [];
+  const randomLength = Math.ceil(Math.random() * 10) + 3;
+  const reviewsArray = [];
   // looping up to the random length
   for (var i = 1; i < randomLength; i++) {
     // random index generator
-    let randomIdx = Math.floor(Math.random() * 100);
+    const randomIdx = Math.floor(Math.random() * 100);
     // push random comments into array using randomIdx
     reviewsArray.push(reviewsList[randomIdx]);
   }
   // returns array of reviews
   return reviewsArray;
-}
+};
 
-let seedDb = function (restaurantsData) {
+const seedDb = function (restaurantsData) {
   // looping thru restaurants
   restaurantsData.forEach((restaurant) => {
     // for a restaurant, use schema to set values
-    let eachRestaurant = {
+    const eachRestaurant = {
       restaurantId: restaurant.id,
       restaurantName: restaurant.name,
-      restaurantReviews: getReviews()
-    }
+      restaurantReviews: getReviews(),
+    };
     // save each one into db
     db.insertOne(eachRestaurant, (error) => {
       if (error) {
@@ -39,4 +39,4 @@ let seedDb = function (restaurantsData) {
 };
 
 // seed data into db
-seedDb(restaurantsData);
+// seedDb(restaurantsData);
