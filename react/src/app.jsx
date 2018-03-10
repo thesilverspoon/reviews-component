ae89d7f7e2e34ed7874b58f6e625bb34d1fc8872
Michael <mqngo21@gmail.com>
2018-03-09 16:52:07 -0800

    updated css

--- react/src/app.jsx
+++ react/src/app.jsx
@@ -36,7 +36,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='appBox'>
         <OverallRatings restaurants={this.state.data} />
         <Restaurants restaurantsList={this.state.data} />
       </div>
